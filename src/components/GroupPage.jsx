b38b89040bd4b0350353bd1790d0e178476917c1
Mohamad Dakdouk <mdak2806@gmail.com>
2022-10-12 15:22:04 +1100

    payments

--- src/components/GroupPage.jsx
+++ src/components/GroupPage.jsx
@@ -1,9 +1,16 @@
-import { useEffect, useState } from 'react'
+import React from "react";
+import '../App.css';
+import ReactDOM from 'react-dom';
+import { useState, useEffect } from "react";
 import { useParams } from 'react-router-dom'
 import { request } from '../utils/request'
+import { useNavigate } from "react-router-dom";
+
 const GroupPage = (props) => {
     const [group, setGroup] = useState(null);
     const [currentUser, setCurrentUser] = useState(props.user);
+    const navigatePush = useNavigate();
+
 
     const { id } = useParams();
 
@@ -12,6 +19,25 @@ const GroupPage = (props) => {
             setGroup(response.data.find(group => group._id === id))
         })
     }, [props.user])
+
+    function handleBack(ev){
+
+        console.log('click');
+        navigatePush(`/groups`);
+
+
+
+    }
+
+    return(
+
+        <div className="content">
+            Hello
+            <button onClick={(ev) => handleBack(ev)}> Back </button>
+        </div>
+
+
+    )
 }
 
 export default GroupPage;
